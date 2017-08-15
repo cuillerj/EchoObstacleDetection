ae99d1edc563d81900ecc2b136ce8a01b1b671cd
cuillerj <jean.cuiller@wanadoo.fr>
2017-08-15 19:19:26 +0200

    added moveAcrosNarrow path

--- EchoObstacleDetection.cpp
+++ EchoObstacleDetection.cpp
@@ -1,17 +1,34 @@
+/*
+version 2 release notes
+cycle modification to trigger alternativly the echos (previously simultanously)
+monitor function added
+*/
 #include "EchoObstacleDetection.h"
 #define maxTimerCycle 4.9   // timer limitation
 #define timerFrequency 15625  // 16000000 arduino frequency 1024 prescaler
+//#define debugOn
+
+boolean echoOn[4];       // store 1 to 4 echo status on off - added 09/08/2017
 uint8_t echoArray[4];       // store 1 to 4 echo pin values
 uint8_t triggerArray[4];    // store 1 to 4 trigger pin values
 boolean alertArray[4];      // alert flags 
+uint8_t actionArray[5];     // store action for each echo and for combination
+boolean monitorFlag=false;  // use to differentiate monitor from simple detection mode
 uint8_t _pinInterrupt;      // pin used tio interrupt main program
-uint8_t lastAlertEchoNumber; // keep last echNumber that was in distance alert    
+uint8_t lastAlertEchoNumber; // keep last echNumber that was in distance alert  
+uint8_t activeEchoNumber=0;  // number of current active echos
+uint8_t activeEchoList[4]={0xffffffff};  // keep track of the  of active echos
+uint8_t currentTriggerID=0;  // which trigger is currently on
 unsigned int tcntEcho ;   // used to init timer overflow for instance 49911 for 1s cycle 
-unsigned long distArray[4];  // keep last echo distances for each echo pin
+unsigned int _equalMargin;  // margin to check echo equlality with some lenght
+unsigned long distArray[5];  // keep last echo distances for each echo pin + a combination
 volatile boolean triggerArrayStatus[4];   // 4 switchs that indicate if echo has been triggered on not
 volatile unsigned long prevEchoMicro[4];  // 4 timers used to calculate distances
-volatile unsigned long lastEchoMicro[4];  // 4 timers that contain last echo duration in ms
-void CheckAlert(uint8_t echoNumber);
+volatile unsigned long lastEchoMicro[5];  // 4 timers that contain last echo duration in ms + combination
+volatile uint8_t retryCompareCount[5];  // 4 timers that contain last echo duration in ms + combination
+volatile uint8_t retryNACount[5];  // 4 timers that contain last echo duration in ms + combination
+void CheckAlert(uint8_t echoNumber);  
+void SetInterruptPIN();
 
 EchoObstacleDetection::EchoObstacleDetection(uint8_t pinEcho1 ,uint8_t pinTrigger1 ,uint8_t pinEcho2 ,uint8_t pinTrigger2 ,uint8_t pinEcho3 ,
 												uint8_t pinTrigger3 ,uint8_t pinEcho4 ,uint8_t pinTrigger4 ,uint8_t pinInterrupt)
@@ -29,72 +46,87 @@ EchoObstacleDetection::EchoObstacleDetection(uint8_t pinEcho1 ,uint8_t pinTrigge
 }
 	void Echo1Interrupt()
 {
-  if (digitalRead(echoArray[0]) == 1)
-  {
-    prevEchoMicro[0] = micros();
-  }
-  else
-  {
-    lastEchoMicro[0] = micros() - prevEchoMicro[0];
-	CheckAlert(0);
-  }
-
+	if (echoOn[0])
+	{
+	  if (digitalRead(echoArray[0]) == 1)
+	  {
+		prevEchoMicro[0] = micros();
+	  }
+	  else
+	  {
+		lastEchoMicro[0] = micros() - prevEchoMicro[0];
+		CheckAlert(0);
+	  }
+	}
 }
 
 	void Echo2Interrupt()
 {
-
-  if (digitalRead(echoArray[1]) == 1)
-  {
-    prevEchoMicro[1] = micros();
-	CheckAlert(1);
-  }
-  else
-  {
-    lastEchoMicro[1] = micros() - prevEchoMicro[1];
-
-  }
+	if (echoOn[1])
+	{
+	  if (digitalRead(echoArray[1]) == 1)
+	  {
+		prevEchoMicro[1] = micros();
+	  }
+	  else
+	  {
+		lastEchoMicro[1] = micros() - prevEchoMicro[1];
+		CheckAlert(1);
+	  }
+	}
 }
 	void Echo3Interrupt()
 {
-  if (digitalRead(echoArray[2]) == 1)
-  {
-    prevEchoMicro[2] = micros();
-	CheckAlert(2);
-  }
-  else
-  {
-    lastEchoMicro[2] = micros() - prevEchoMicro[2];
-  }
+	if (echoOn[2])
+	{
+	  if (digitalRead(echoArray[2]) == 1)
+	  {
+		prevEchoMicro[2] = micros();
+	  }
+	  else
+	  {
+		lastEchoMicro[2] = micros() - prevEchoMicro[2];
+		CheckAlert(2);
+	  }
+	}
 }
 	void Echo4Interrupt()
 {
-  if (digitalRead(echoArray[3]) == 1)
-  {
-    prevEchoMicro[3] = micros();
-	CheckAlert(3);
-  }
-  else
-  {
-    lastEchoMicro[3] = micros() - prevEchoMicro[3];
-  }
-}
-ISR(TIMER4_OVF_vect)        // timer interrupt used to regurarly trigger echos
-{
-  TCNT4 = tcntEcho;            // preload timer to adjust duration
-	for (uint8_t idx=0;idx<4;idx++)
+	if (echoOn[3])
 	{
-		if (echoArray[idx]!=0)
-		{
-			  digitalWrite(triggerArray[idx], LOW);  //
-			  delayMicroseconds(2);                  // 
-			  digitalWrite(triggerArray[idx], HIGH);  //			  
-			  delayMicroseconds(15); 				// 10 micro sec mini
-			  digitalWrite(triggerArray[idx], HIGH);  //
-			  triggerArrayStatus[idx]=true;
-	
+	  if (digitalRead(echoArray[3]) == 1)
+	  {
+		prevEchoMicro[3] = micros();
+	  }
+	  else
+	  {
+		lastEchoMicro[3] = micros() - prevEchoMicro[3];
+		CheckAlert(3);		
+	  }
+	}
+}
+ISR(TIMER4_OVF_vect)       
+/*
+ this timer interrupt is used to regurarly trigger echos for active sensors
+*/
+{
+	TCNT4 = tcntEcho;            // preload timer to adjust duration
 
-		}
+#if defined(debugOn)
+			Serial.print("T:");
+			Serial.print(" ");
+			Serial.print(activeEchoNumber);
+			Serial.print(" ");
+			Serial.println(currentTriggerID);
+#endif
+			  digitalWrite(triggerArray[currentTriggerID], LOW);  //
+			  digitalWrite(triggerArray[currentTriggerID], HIGH);  //			  
+			  delayMicroseconds(11); 				// 10 micro sec mini for the sensor
+			  digitalWrite(triggerArray[currentTriggerID], LOW);  //
+			  triggerArrayStatus[currentTriggerID]=true;
+	if(activeEchoNumber>0)
+	{
+		currentTriggerID=(currentTriggerID+1)%(activeEchoNumber);	// set currentTriggerID to the next active one
 	}
 }
 
@@ -109,33 +141,50 @@ ISR(TIMER4_OVF_vect)        // timer interrupt used to regurarly trigger echos
 	{
 		cycleDuration = maxTimerCycle;
 	}
-		
+	monitorFlag=false;
 	tcntEcho=65536-timerFrequency*cycleDuration;  // tcntEcho intialised to such a value that timer overflow will appear after the cycle duration
-
-		
-	if (echo1)
+	activeEchoNumber=0;
+	uint8_t activeListIdx=0;	
+	if (echo1 && !echoOn[0])
 	{
 		echoArray[0]=_pinEcho1;
 		triggerArray[0]=_pinTrigger1;
 		triggerArrayStatus[0]=false;
+		echoOn[0]=true;
+		activeEchoList[activeListIdx]=0x00;
+		activeEchoNumber++;
+		activeListIdx++;
 	}
-	if (echo2)
+	if (echo2 && !echoOn[1])
 	{
 		echoArray[1]=_pinEcho2;
 		triggerArray[1]=_pinTrigger2;
 		triggerArrayStatus[1]=false;
+		echoOn[1]=true;
+		activeEchoList[activeListIdx]=0x01;
+		activeEchoNumber++;
+		activeListIdx++;
 	}
-	if (echo3)
+	if (echo3 && !echoOn[2])
 	{
 		echoArray[2]=_pinEcho3;
 		triggerArray[2]=_pinTrigger3;
 		triggerArrayStatus[2]=false;
+		echoOn[2]=true;
+		activeEchoList[activeListIdx]=0x02;
+		activeEchoNumber++;
+		activeListIdx++;
 	}
-	if (echo4)
+	if (echo4&& !echoOn[3])
 	{
 		echoArray[3]=_pinEcho4;
 		triggerArray[3]=_pinTrigger4;
 		triggerArrayStatus[3]=false;
+		echoOn[3]=true;
+		activeEchoList[3]=true;
+		activeEchoList[activeListIdx]=0x03;
+		activeEchoNumber++;
+		activeListIdx++;
 	}
 	for (uint8_t idx=0;idx<4;idx++)
 	{
@@ -169,54 +218,135 @@ ISR(TIMER4_OVF_vect)        // timer interrupt used to regurarly trigger echos
 	TIMSK4 |= (1 << TOIE4); // enable timer overflow interrupt
 	interrupts(); // enable all interrupts
 }
-   void EchoObstacleDetection::StopDetection(boolean echo1, boolean echo2, boolean echo3, boolean echo4)
+void EchoObstacleDetection::SetMonitorOn(boolean echo1,unsigned int dist1,uint8_t action1, boolean echo2,unsigned int dist2,uint8_t action2,boolean echo3,
+				  unsigned int dist3,uint8_t action3,boolean echo4,unsigned int dist4,uint8_t action4,uint8_t actionSum,unsigned int distSumm,unsigned int equalMargin)
 {
-		if (echo1)
+
+#if defined(debugOn)
+	Serial.print("*start monitor:");
+	Serial.print(echo1);
+	Serial.print(" ");
+	Serial.print(echo2);
+	Serial.print(" ");
+    Serial.print(echo3);
+	Serial.print(" ");
+    Serial.println(echo4);
+#endif		
+	if (echo1 )
 	{
-		echoArray[0]=0x00;
-		triggerArray[0]=0x00;
-		triggerArrayStatus[0]=false;
-		detachInterrupt(digitalPinToInterrupt(echoArray[0]));
-		lastEchoMicro[0]=0;
-		alertArray[0]=false;
+		actionArray[0]=action1;
+		alertArray[0]=true;
+		distArray[0]=dist1;
+		retryCompareCount[0]=0x00;
 	}
-		if (echo2)
+	if (echo2 )
 	{
-		echoArray[1]=0x00;
-		triggerArray[1]=0x00;
-		triggerArrayStatus[1]=false;
-		detachInterrupt(digitalPinToInterrupt(echoArray[1]));
-		lastEchoMicro[1]=0;
-		alertArray[1]=false;
+		actionArray[1]=action2;
+		alertArray[1]=true;
+		distArray[1]=dist2;
+		retryCompareCount[1]=0x00;
 	}
-		if (echo3)
+	if (echo3 )
 	{
-		echoArray[2]=0x00;
-		triggerArray[2]=0x00;
-		triggerArrayStatus[2]=false;
-		detachInterrupt(digitalPinToInterrupt(echoArray[2]));
-		lastEchoMicro[2]=0;
-		alertArray[2]=false;
+		actionArray[2]=action3;
+		alertArray[2]=true;
+		distArray[2]=dist3;
+		retryCompareCount[2]=0x00;
 	}
-		if (echo4)
+	if (echo4 )
 	{
-		echoArray[3]=0x00;
-		triggerArray[3]=0x00;
-		triggerArrayStatus[3]=false;
-		detachInterrupt(digitalPinToInterrupt(echoArray[3]));
-		lastEchoMicro[3]=0;
-		alertArray[3]=false;
+		actionArray[3]=action4;
+		alertArray[3]=true;
+		distArray[3]=dist4;
+		retryCompareCount[3]=0x00;
 	}
-		uint8_t countInt=0;
-		for (int i=0;i<4;i++)
-			{
-				if (triggerArrayStatus[i]==true)
-					{
-						countInt++;
-					}
-			}
-		if (countInt==0)  // stop timer
-			{
+	actionArray[pseudoEcho]=actionSum;
+	distArray[pseudoEcho]=distSumm;
+	retryCompareCount[pseudoEcho]=0x00;
+	_equalMargin=equalMargin;
+	monitorFlag=true;
+	for (uint8_t idx=0;idx<4;idx++)
+	{
+		if (echoArray[idx]!=0)
+		{
+			pinMode(echoArray[idx], INPUT);
+			pinMode(triggerArray[idx], OUTPUT);
+#if defined(debugOn)
+	Serial.print(echoArray[idx]);
+	Serial.print("/");
+	Serial.println(triggerArray[idx]);		
+#endif	
+		}	
+
+		if (echoArray[0]!=0)
+		{
+			attachInterrupt(digitalPinToInterrupt(echoArray[0]), Echo1Interrupt, CHANGE);
+		}
+		if (echoArray[1]!=0)
+		{
+			attachInterrupt(digitalPinToInterrupt(echoArray[1]), Echo2Interrupt, CHANGE);
+		}
+		if (echoArray[2]!=0)
+		{
+			attachInterrupt(digitalPinToInterrupt(echoArray[2]), Echo3Interrupt, CHANGE);
+		}
+			if (echoArray[3]!=0)
+		{
+			attachInterrupt(digitalPinToInterrupt(echoArray[3]), Echo4Interrupt, CHANGE);
+		}
+	}
+	/*
+	noInterrupts(); // disable all interrupts
+	TCCR4A = 0;  // set entire TCCR4A register to 0
+	TCCR4B = 0;  // set entire TCCR4B register to 0
+	TCNT4 = tcntEcho; // 
+	TCCR4B |= ((1 << CS12) | (1 << CS10)); // 1024 prescaler
+	TIMSK4 |= (1 << TOIE4); // enable timer overflow interrupt
+	interrupts(); // enable all interrupts	
+	*/
+ }
+    void EchoObstacleDetection::SetMonitorOff()
+	{
+		for (uint8_t i=0;i<4;i++)
+		{
+		alertArray[i]=false;
+		distArray[i]=0;
+		echoArray[i]=0x00;
+		triggerArray[i]=0x00;
+		actionArray[i]=0x00;
+		triggerArrayStatus[i]=false;
+		detachInterrupt(digitalPinToInterrupt(echoArray[i]));
+		lastEchoMicro[i]=0;
+		alertArray[i]=false;
+//		echoOn[i]=false;
+		}
+		distArray[pseudoEcho]=0;	
+		actionArray[pseudoEcho]=0x00;
+		monitorFlag=false;
+//		activeEchoNumber=0;
+	}
+   void EchoObstacleDetection::StopDetection()
+{
+	#if defined(debugOn)
+	Serial.print("stop detection");
+#endif	
+//		if (echo1 && echoOn[0])
+//	{
+	for (int i=0;i<4;i++)
+	{
+		echoArray[i]=0x00;
+		triggerArray[i]=0x00;
+		triggerArrayStatus[i]=false;
+		detachInterrupt(digitalPinToInterrupt(echoArray[i]));
+		lastEchoMicro[i]=0;
+		alertArray[i]=false;
+		echoOn[i]=false;
+	}
+		activeEchoNumber=0;
+
+#if defined(debugOn)
+	Serial.print("stop timer interrupt");
+#endif	
 			noInterrupts(); // disable all interrupts
 			TCCR4A = 0;  // set entire TCCR5A register to 0
 			TCCR4B = 0;  // set entire TCCR5B register to 0
@@ -226,7 +356,7 @@ ISR(TIMER4_OVF_vect)        // timer interrupt used to regurarly trigger echos
 			TCCR4B |= ((0 << CS12) ); // 256 prescaler
 			TIMSK4 |= (0 << TOIE4); // enable timer overflow interrupt
 			interrupts(); // enable all interrupts
-			}
+	//		}
 }
    void EchoObstacleDetection::SetAlertOn(boolean echo1,unsigned int dist1,boolean echo2,unsigned int dist2,boolean echo3,unsigned int dist3,boolean echo4,unsigned int dist4)
  {
@@ -290,28 +420,218 @@ unsigned int EchoObstacleDetection::GetDistance(uint8_t echoNumber)
 		retCode= lastEchoMicro[echoNumber]/echoCm;
 		if (retCode > echoMaxValue)
 		{
-			retCode=0;
-		}
-		
+			retCode=999;
+		}		
 	}
 
 	return retCode;
-
 }
 void CheckAlert(uint8_t echoNumber)
 {
-	if (lastEchoMicro[echoNumber]/echoCm < distArray[echoNumber])
+#if defined(debugOn)
+	Serial.print(".");
+#endif	
+	if (!monitorFlag)     // simple obstacle single detection mode
 	{
-		if (_pinInterrupt!=0)
+#if defined(debugOn)
+	Serial.print("-");
+#endif	
+		if (lastEchoMicro[echoNumber]/echoCm <= distArray[echoNumber]) // modified 09082017 to allow monitoring distance becoming 0
 		{
-			lastAlertEchoNumber=echoNumber;
-			pinMode(_pinInterrupt,OUTPUT);		
-			digitalWrite(_pinInterrupt,1);
-			delay(1);
-			digitalWrite(_pinInterrupt,0);
+			SetInterruptPIN();
+			/*
+			if (_pinInterrupt!=0)
+			{
+				lastAlertEchoNumber=echoNumber;
+				pinMode(_pinInterrupt,OUTPUT);		
+				digitalWrite(_pinInterrupt,1);
+				delay(1);
+				digitalWrite(_pinInterrupt,0);
+			}
+			*/
+	//		digitalWrite(_pinInterrupt,1);
 		}
-//		digitalWrite(_pinInterrupt,1);
 	}
+	else                  // monitor mode
+	{
+#if defined(debugOn)
+	Serial.print("+");
+	Serial.print(echoNumber);
+	Serial.print("+");
+	Serial.print(lastEchoMicro[echoNumber]);
+	Serial.print("a");
+	Serial.print(actionArray[echoNumber],HEX);
+	Serial.print("r");
+	Serial.print(retryCompareCount[echoNumber]);
+	Serial.print("d");
+	Serial.println(distArray[echoNumber]);
+#endif	
+		boolean detected=false;
+		if( bitRead(actionArray[echoNumber],actionEqualNA))
+		{
+			if (lastEchoMicro[echoNumber]/echoCm >=echoMaxValue) 
+			{
+					retryNACount[echoNumber]++;
+					if (retryNACount[echoNumber] >=retryCompareNA)
+					{
+						detected=true;
+						lastAlertEchoNumber=echoNumber;
+						SetInterruptPIN();
+					}
+			}
+			else
+			{
+				retryNACount[echoNumber]=0x00;
+			}	
+		}
+		if( bitRead(actionArray[echoNumber],actionUpper))
+		{
+			if (lastEchoMicro[echoNumber]/echoCm >= distArray[echoNumber]) 
+			{
+					retryCompareCount[echoNumber]++;
+					if (retryCompareCount[echoNumber] >=retryCompareValue)
+					{
+						detected=true;
+						lastAlertEchoNumber=echoNumber;
+						SetInterruptPIN();
+					}					
+
+			}
+			else
+			{
+				retryCompareCount[echoNumber]=0x00;
+			}				
+		}
+		if( bitRead(actionArray[echoNumber],actionLower))
+		{
+			if (lastEchoMicro[echoNumber]/echoCm <= distArray[echoNumber]) 
+			{
+					retryCompareCount[echoNumber]++;
+					if (retryCompareCount[echoNumber] >=retryCompareValue)
+					{
+						detected=true;
+						lastAlertEchoNumber=echoNumber;
+						SetInterruptPIN();
+					}					
+
+			}			
+			else
+			{
+				retryCompareCount[echoNumber]=0x00;
+			}	
+		}
+		/*
+		below code for echo combination using pseudo echo
+		actionArray[pseudoEcho] bits map with the echos to summ
+		*/		
+		if( bitRead(actionArray[pseudoEcho],echoNumber))  
+		{
+			int summEcho=0;
+			for (uint8_t i=0;i<4;i++)
+			{
+				if( bitRead(actionArray[pseudoEcho],i))				
+				{
+					summEcho=summEcho+lastEchoMicro[i]/echoCm;
+
+				}
+#if defined(debugOn)
+					Serial.print("> ");
+					Serial.print(i);
+					Serial.print(" ");
+					Serial.print(lastEchoMicro[i]/echoCm);
+					Serial.print(" ");
+					Serial.print("summ: ");
+					Serial.print(summEcho);
+					Serial.print(" ");
+					Serial.print(retryCompareCount[pseudoEcho]);
+					Serial.print(" 0x");
+					Serial.print(actionArray[pseudoEcho],HEX);
+				Serial.println();
+#endif
+			}
+
+			if (bitRead(actionArray[pseudoEcho],actionLower))
+			{
+				if (summEcho<= distArray[pseudoEcho]) 
+				{
+					retryCompareCount[pseudoEcho]++;
+					if (retryCompareCount[pseudoEcho]=retryCompareValue)
+					{
+						detected=true;	
+					}
+				}
+				else{
+					retryCompareCount[pseudoEcho]=0x00;
+				}
+			}	
+			
+			if (bitRead(actionArray[pseudoEcho],actionUpper) && !detected)
+			{
+				if (summEcho>= distArray[pseudoEcho]) 
+				{
+					retryCompareCount[pseudoEcho]++;
+					if (retryCompareCount[pseudoEcho]>=retryCompareValue)
+					{
+						detected=true;	
+					}
+				}
+				else{
+					retryCompareCount[pseudoEcho]=0x00;
+				}
+			}	
+
+			if (bitRead(actionArray[pseudoEcho],actionEqual)&& !detected)
+			{
+				if (summEcho<= distArray[pseudoEcho]+_equalMargin && summEcho>= distArray[pseudoEcho]-_equalMargin) 
+				{
+					retryCompareCount[pseudoEcho]++;
+					if (retryCompareCount[pseudoEcho]>=retryCompareValue)
+					{
+						detected=true;	
+					}
+				}
+				else{
+					retryCompareCount[pseudoEcho]=0x00;
+				}
+			}				
+	
+			if (bitRead(actionArray[pseudoEcho],actionNotEqual)&& !detected)
+			{
+				if ((summEcho>= distArray[pseudoEcho]+_equalMargin || summEcho<= distArray[pseudoEcho]-_equalMargin)) 
+				{
+					retryCompareCount[pseudoEcho]++;
+					if (retryCompareCount[pseudoEcho]>=retryCompareValue)
+					{
+						detected=true;
+					}
+				}
+				else{
+					retryCompareCount[pseudoEcho]=0x00;
+				}
+			}	
+
+			if (detected)
+			{
+					lastAlertEchoNumber=pseudoEcho;
+					lastEchoMicro[pseudoEcho]=summEcho*echoCm;
+					SetInterruptPIN();
+			}			
+		}
+	
+	}
+}
+void SetInterruptPIN()
+{
+#if defined(debugOn)
+	Serial.println("I");
+#endif
+	if (_pinInterrupt!=0)
+		{
+		pinMode(_pinInterrupt,OUTPUT);		
+		digitalWrite(_pinInterrupt,1);
+		delay(1);
+		digitalWrite(_pinInterrupt,0);
+		}
 }
 uint8_t EchoObstacleDetection::GetAlertEchoNumber()
 {
--- EchoObstacleDetection.h
+++ EchoObstacleDetection.h
@@ -6,7 +6,7 @@ first instantiate the object to define pins used for triger and echo measurement
 	4 x couples(echo pin,trigger pin) must be defined - 0 meaning HCRS does not exist or will not used	
 	1 x interrupt pin must be defined - 0 meaning interrupt will not be used
 Start Detection is called to activate HCSR devices	
-	1 to 4 HSCR can be activated simultaneously but echo must not interact eachother (true active, false inactive)	
+	1 to 4 HSCR can be activated  (true active, false inactive)	
 	cycleDuration define delay between 2 triggers en seconds - must remain between 0.1s to 4.9s
 SetAlertOn is used to activate interruption (the main code must attach this interrupt) when distances are below defined thresholds
 	4 x couples (true, threshold) must be defined - (false,0) meaning not used
@@ -15,14 +15,36 @@ GetDistance provide last distance mesurment for the specified HCSR
 GetEchoThreshold provide threshold distance for the specified HCSR under which an interrupt will be set on
 SetAlertOff switch off alert for HSCR that are no longer in true state
 StopDetection stop detection for HSCR that are no longer in true state
+SetMonitorOn activate the monitor function
+SetMonitorOff desactivate the monitor function
 
+
+Typical usage:
+	EchoObstacleDetection() to define the object and hardware
+	StartDetection() to activate the sensors that wil be used
+	SetAlertOn() to simply detect objects
+		or
+	SetMonitorOn() to monitor more complex echos
+
+SetAlertOn() will permently check that one or more echos will be over a threshold - if not will raise an interrupt
+
+SetMonitorOn() will permently check that one or more echos or the summ of them will be over, under or equal to a threshold - if not will raise an interrupt
 */
 #include <Arduino.h>
 #ifndef echoObstacleDetection_h_included
 #define echoObstacleDetection_h_included
 #define echoCm 58           // sound speed 
-#define echoMaxValue 450    // 450 cm max for hsr04
+#define echoMaxValue 400    // 400 cm max for hsr04
 #define minTimerCycle 0.1   // echo round trip limitation
+#define actionEqualNA 0     // action corresponding to we got no available distance from the sensor
+#define actionNotEqual 4    // action corresponding to we got distance from the sensor that is not equal to the threshold (+- the margin)
+#define actionEqual 5		// action corresponding to we got distance from the sensor that is equal to the threshold (+- the margin)
+#define actionUpper 6       // action corresponding to we got distance from the sensor that is over the threshold
+#define actionLower 7       // action corresponding to we got distance from the sensor that is under the threshold
+#define pseudoEcho 4       // pseudo echo used to summ 2 or more echos
+#define minusAction 7      // define action bit to set minus or add 
+#define retryCompareValue 3   // echos will be checked retryCompareValue time before raising an interrupt in case of comparison
+#define retryCompareNA 2    // echos will be checked retryCompareValue time before raising an interrupt in case of no available distance
 
 class EchoObstacleDetection
 {
@@ -30,9 +52,12 @@ public:
 EchoObstacleDetection(uint8_t pinEcho1 ,uint8_t pinTrigger1 ,uint8_t pinEcho2 ,uint8_t pinTrigger2 ,uint8_t pinEcho3 ,
 					uint8_t pinTrigger3 ,uint8_t pinEcho4 ,uint8_t pinTrigger4 ,uint8_t pinInterrupt);
 void StartDetection(boolean echo1,boolean echo2,boolean echo3,boolean echo4,float cycleDuration);
-void StopDetection(boolean echo1,boolean echo2,boolean echo3,boolean echo4);
+void StopDetection();
 void SetAlertOn(boolean echo1,unsigned int dist1,boolean echo2,unsigned int dist2,boolean echo3,unsigned int dist3,boolean echo4,unsigned int dist4);
 void SetAlertOff(boolean echo1,boolean echo2,boolean echo3,boolean echo4);
+void SetMonitorOn(boolean echo1,unsigned int dist1,uint8_t action1, boolean echo2,unsigned int dist2,uint8_t action2,boolean echo3,
+				  unsigned int dist3,uint8_t action3,boolean echo4,unsigned int dist4,uint8_t action4,uint8_t actionSum,unsigned int distSum,unsigned int equalMargin);
+void SetMonitorOff();
 unsigned int GetDistance(uint8_t  echoNumber);
 unsigned int GetEchoThreshold(uint8_t  echoNumber);
 uint8_t GetAlertEchoNumber();
